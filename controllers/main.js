c446f26c53b8022edf4c18f308dafd1be657524a
Alish Bista <bistace321@gmail.com>
2023-09-01 19:33:54 +0545

    dev: controller initated

---
+++ controllers/main.js
@@ -0,0 +1,11 @@
+
+const login = async (req, res) => {
+    res.send('Fake Login/Register/Sign Route')
+}
+
+
+const dashboard = async (req, res) => {
+    const luckyNumber = Math.floor(Math.random()*100)
+    res.status(200).json({msg: `Hello, John DOe`, secret: `Here is your authorized data, your lucky number is ${luckyNumber}`})
+}
+
