c86edf7d4526d52ff74f047fd08aebea15d44c53
Alish Bista <bistace321@gmail.com>
2023-09-01 19:43:53 +0545

    dev: mongoose connected

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ require('express-async-errors');
 
 const express = require('express');
 const app = express();
-
+const connectDB = require('./db/connect');
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
@@ -18,6 +18,7 @@ const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
+    await connectDB(process.env.MONGO_URI)
     app.listen(port, () =>
       console.log(`Server is listening on port ${port}...`)
     );
