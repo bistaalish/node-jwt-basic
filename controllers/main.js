0e4c44750c4ad7d82b1a8602aa6796757bf614aa
Alish Bista <bistace321@gmail.com>
2023-09-02 18:49:23 +0545

    feat: middleware set

--- controllers/main.js
+++ controllers/main.js
@@ -4,7 +4,6 @@ const jwt = require('jsonwebtoken')
 const login =  async (req, res) => {
     // res.send('Fake Login/Register/Sign Route')
     const {username,password} = req.body
-    console.log(username,password)
     // check username and password
      // Mongo
     
@@ -16,35 +15,33 @@ const login =  async (req, res) => {
     }
 
 
-    const token = jwt.sign({id,username},process.env.SECRET_KET,{
+    const token = jwt.sign({id,username},process.env.SECRET_KEY,{
         expiresIn: '7d'
     })
-    console.log(token)
-   
     res.status(201).json({msg: 'user created',token})
 }
 
 
 const dashboard =  async (req, res) => {
-    const authHeader = req.headers.authorization;
-    if (!authHeader || !authHeader.startsWith('Bearer ')) {
-        throw new CustomAPIError('No token provided',403)
-      }
+    // const authHeader = req.headers.authorization;
+    // if (!authHeader || !authHeader.startsWith('Bearer ')) {
+    //     throw new CustomAPIError('No token provided',403)
+    //   }
     
 
-    const token = authHeader.split(' ')[1]
-    console.log(token)
+    // const token = authHeader.split(' ')[1]
+    // console.log(token)
 
-    try {
-        const decoded = jwt.verify(token,process.env.SECRET_KET)
-        console.log(decoded)
-        res.status(200).json({decoded})
-    } catch (error) {
-        throw new CustomAPIError('Not authorized to access this route',401)
+    // try {
+    //     const decoded = jwt.verify(token,process.env.SECRET_KET)
+    //     console.log(decoded)
+    //     res.status(200).json({decoded})
+    // } catch (error) {
+    //     throw new CustomAPIError('Not authorized to access this route',401)
 
-    }
+    // }
     const luckyNumber = Math.floor(Math.random()*100)
-    res.status(200).json({msg: `Hello, John DOe`, 
+    res.status(200).json({msg: `${req.user.username}`, 
     secret: `Here is your authorized data, your lucky number is ${luckyNumber}`})
 }
 
